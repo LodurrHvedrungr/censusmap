10f474de891cc9f04a8de5b876f2e206e8dfff28
LodurrWasTaken <mrr3alme@gmail.com>
2019-08-13 15:41:48 +0300

    search

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../styles/index.scss'
 
 import mapboxgl from 'mapbox-gl'
+import polygonCenter from 'geojson-polygon-center'
 
 const options = {
     container: 'map',
@@ -9,7 +10,6 @@ const options = {
     zoom: 0,
     maxZoom: 8,
     center: [10.4660513, 50.6702632],
-    // maxBounds: [[5.864417, 47.26543], [15.05078, 55.14777]]
     maxBounds: [[-0.765531, 46.631365], [20.888930, 55.352543]]
 }
 
@@ -20,8 +20,9 @@ const mapHtml = document.querySelector('#map')
 const rampGradient = document.querySelector('.ramp__gradient')
 const negativeValue = document.querySelector('.negative-value')
 const positiveValue = document.querySelector('.positive-value')
+const searchInput = document.querySelector('#search')
 let hoveredStateId = null
-let firstSymbolId
+let marker, firstSymbolId, regionsData = {}
 const colorSchemes = {
     'green-red': [
         -5, 'rgb(255, 26, 26)',
@@ -77,6 +78,88 @@ const colorSchemes = {
     ],
 }
 
+function autocomplete(inp, arr) {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      this.parentNode.appendChild(a);
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+              b.addEventListener("click", function(e) {
+              inp.value = this.getElementsByTagName("input")[0].value;
+              findLocation(inp.value)
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        currentFocus++;
+        addActive(x);
+      } else if (e.keyCode == 38) {
+        currentFocus--;
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentFocus > -1) {
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+function findLocation(loc) {
+    if (marker) marker.remove()
+
+    const center = polygonCenter(regionsData[loc])
+    map.setZoom(8)
+    map.panTo(center.coordinates)
+    let el = document.createElement('div')
+    el.className = 'marker'
+    marker = new mapboxgl.Marker(el)
+        .setLngLat({
+            lat: center.coordinates[1] + 0.05000,
+            lng: center.coordinates[0] 
+        })
+        .addTo(map)
+}
+
 let colorsHtml = ''
 Object.keys(colorSchemes).forEach(color => {
     colorsHtml += `<option value="${color}">${color}</option>`
@@ -109,6 +192,12 @@ colorSelect.addEventListener('change', function (e) {
     positiveValue.style.color = colorSchemes[e.target.value][21]
 })
 
+searchInput.addEventListener('keydown', function (e) {
+    if (e.code === 'Enter') {
+        findLocation(searchInput.value)
+    }
+})
+
 map.on('load', () => {
     map.resize()
 
@@ -119,29 +208,38 @@ map.on('load', () => {
             break;
         }
     }
+    fetch('data.json')
+            .then(r => r.json())
+            .then(r => {
+                map.addSource('population', {
+                    type: 'geojson',
+                    data: r
+                })
+                map.addLayer({
+                    'id': 'state-population',
+                    'source': 'population',
+                    'type': 'fill',
+                    'paint': {
+                        'fill-color': [
+                            'interpolate',
+                            ['linear'],
+                            ['get', 'JVA'],
+                            ...colorSchemes['green-red']
+                        ],
+                        "fill-opacity": ["case",
+                            ["boolean", ["feature-state", "hover"], false],
+                            0.8,
+                            0.6
+                        ]
+                    }
+                }, firstSymbolId)
 
-    map.addSource('population', {
-        type: 'geojson',
-        data: 'https://lodurrhvedrungr.github.io/censusjson/data.json'
-    })
-    map.addLayer({
-        'id': 'state-population',
-        'source': 'population',
-        'type': 'fill',
-        'paint': {
-            'fill-color': [
-                'interpolate',
-                ['linear'],
-                ['get', 'JVA'],
-                ...colorSchemes['green-red']
-            ],
-            "fill-opacity": ["case",
-                ["boolean", ["feature-state", "hover"], false],
-                0.8,
-                0.6
-            ]
-        }
-    }, firstSymbolId)
+                regionsData = r.features.reduce((acc, feat) => {
+                    acc[`${feat.properties.GEN}, ${feat.properties.BEZ}`] = feat.geometry
+                    return acc
+                }, {})
+                autocomplete(searchInput, Object.keys(regionsData))
+            })
 
     map.on('mousemove', 'state-population', function (e) {
         const d = map.queryRenderedFeatures(e.point)
@@ -218,7 +316,5 @@ function createPopupMsg(data) {
 }
 
 function generateLegendGradient(currentColor) {
-    console.log(currentColor)
-    console.log(colorSchemes[currentColor])
     return `linear-gradient(to right, ${colorSchemes[currentColor][1]} 0%, ${colorSchemes[currentColor][5]} 33%, ${colorSchemes[currentColor][9]} 49%, ${colorSchemes[currentColor][11]} 50%, ${colorSchemes[currentColor][13]} 51%, ${colorSchemes[currentColor][17]} 67%, ${colorSchemes[currentColor][21]} 100%)`
 }
