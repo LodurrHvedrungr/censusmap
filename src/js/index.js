1f667ec31983b31a27fc76cbcb64fd2d37b3a406
LodurrHvedrungr <evvmoroz@gmail.com>
2019-08-14 14:49:04 +0300

    1st milestone fixes

--- src/js/index.js
+++ src/js/index.js
@@ -9,105 +9,14 @@ const options = {
     zoom: 0,
     maxZoom: 8,
     center: [10.4660513, 50.6702632],
-    // maxBounds: [[5.864417, 47.26543], [15.05078, 55.14777]]
     maxBounds: [[-0.765531, 46.631365], [20.888930, 55.352543]]
 }
 
-const colorSelect = document.getElementById('color-switcher')
 const map = new mapboxgl.Map(options)
 const popup = document.querySelector('.popup')
 const mapHtml = document.querySelector('#map')
-const rampGradient = document.querySelector('.ramp__gradient')
-const negativeValue = document.querySelector('.negative-value')
-const positiveValue = document.querySelector('.positive-value')
 let hoveredStateId = null
 let firstSymbolId
-const colorSchemes = {
-    'green-red': [
-        -5, 'rgb(255, 26, 26)',
-        -3, 'rgb(230, 0, 0)',
-        -1.5, 'rgb(179, 0, 0)',
-        -1, 'rgb(128, 0, 0)',
-        -0.1, 'rgb(77, 0, 0)',
-        0, '#000000',
-        0.1, 'rgb(0, 77, 19)',
-        1, 'rgb(0, 128, 31)',
-        1.5, 'rgb(0, 179, 43)',
-        3, 'rgb(0, 230, 56)',
-        5, 'rgb(26, 255, 81)'
-    ],
-    'purple-yellow': [
-        -5, 'rgb(241, 255, 26)',
-        -3, 'rgb(216, 230, 0)',
-        -1.5, 'rgb(168, 179, 0)',
-        -1, 'rgb(120, 128, 0)',
-        -0.1, 'rgb(72, 77, 0)',
-        0, '#000000',
-        0.1, 'rgb(43, 0, 77)',
-        1, 'rgb(71, 0, 128)',
-        1.5, 'rgb(100, 0, 179)',
-        3, 'rgb(129, 0, 230)',
-        5, 'rgb(154, 26, 255)',
-    ],
-    'blue-green': [
-        -5, 'rgb(98, 134, 44)',
-        -3, 'rgb(125, 173, 57)',
-        -1.5, 'rgb(151, 198, 82)',
-        -1, 'rgb(174, 211, 121)',
-        -0.1, 'rgb(197, 223, 159)',
-        0, '#ffffff',
-        0.1, 'rgb(153, 191, 229)',
-        1, 'rgb(113, 165, 219)',
-        1.5, 'rgb(72, 140, 208)',
-        3, 'rgb(47, 114, 183)',
-        5, 'rgb(36, 89, 142)',
-    ],
-    'orange-lightblue': [
-        -5, 'rgb(0, 143, 179)',
-        -3, 'rgb(0, 184, 230)',
-        -1.5, 'rgb(26, 209, 255)',
-        -1, 'rgb(77, 219, 255)',
-        -0.1, 'rgb(128, 229, 255)',
-        0, '#ffffff',
-        0.1, 'rgb(237, 173, 146)',
-        1, 'rgb(229, 140, 102)',
-        1.5, 'rgb(222, 107, 58)',
-        3, 'rgb(197, 81, 33)',
-        5, 'rgb(153, 63, 26)',
-    ],
-}
-
-let colorsHtml = ''
-Object.keys(colorSchemes).forEach(color => {
-    colorsHtml += `<option value="${color}">${color}</option>`
-})
-colorSelect.innerHTML = colorsHtml
-
-colorSelect.addEventListener('change', function (e) {
-    map.removeLayer('state-population')
-    map.addLayer({
-        'id': 'state-population',
-        'source': 'population',
-        'type': 'fill',
-        'paint': {
-            'fill-color': [
-                'interpolate',
-                ['linear'],
-                ['get', 'JVA'],
-                ...colorSchemes[e.target.value]
-            ],
-            "fill-opacity": ["case",
-                ["boolean", ["feature-state", "hover"], false],
-                0.8,
-                0.6
-            ]
-        }
-    }, firstSymbolId)
-
-    rampGradient.style.background = generateLegendGradient(e.target.value)
-    negativeValue.style.color = colorSchemes[e.target.value][1]
-    positiveValue.style.color = colorSchemes[e.target.value][21]
-})
 
 map.on('load', () => {
     map.resize()
@@ -119,6 +28,11 @@ map.on('load', () => {
             break;
         }
     }
+
+    map.setPaintProperty('country-label', 'text-color', '#000000')
+    map.setPaintProperty('state-label', 'text-color', '#000000')
+    map.setPaintProperty('settlement-label', 'text-color', '#000000')
+    map.setPaintProperty('settlement-subdivision-label', 'text-color', '#000000')
 
     map.addSource('population', {
         type: 'geojson',
@@ -133,12 +47,16 @@ map.on('load', () => {
                 'interpolate',
                 ['linear'],
                 ['get', 'JVA'],
-                ...colorSchemes['green-red']
+                -5, '#0f7785',
+                -0.1, '#8fd7d2',
+                0, '#ffffff',
+                0.1, '#eb9878',
+                5, '#96210e',
             ],
-            "fill-opacity": ["case",
+            "fill-outline-color": ["case",
                 ["boolean", ["feature-state", "hover"], false],
-                0.8,
-                0.6
+                '#18758D',
+                'transparent'
             ]
         }
     }, firstSymbolId)
@@ -166,25 +84,30 @@ map.on('load', () => {
             const bottom = y > mapHtml.clientHeight / 2
             const right = x > mapHtml.clientWidth / 2
 
-            if (bottom && !right) {
-                // bottom left
-                popup.style.left = `${x}px`
-                popup.style.top = `${y - popup.offsetHeight}px`
-            } else if (bottom && right) {
-                // bottom right
-                popup.style.left = `${x - popup.offsetWidth}px`
-                popup.style.top = `${y - popup.offsetHeight}px`
-            } else if (!bottom && !right) {
-                // top left
-                popup.style.left = `${x}px`
-                popup.style.top = `${y}px`
-            } else {
-                // top right
-                popup.style.left = `${x - popup.offsetWidth}px`
-                popup.style.top = `${y}px`
-            }
             popup.innerHTML = createPopupMsg(mainLayer)
             popup.style.display = 'block'
+            const popupArrow = document.querySelector('.popup__arrow')
+
+            if (bottom && !right) {
+                // bottom left
+                popup.style.left = `${x - 30}px`
+                popup.style.top = `${y - popup.offsetHeight - 30}px`
+            } else if (bottom && right) {
+                // bottom right
+                popup.style.left = `${x - popup.offsetWidth + 30}px`
+                popup.style.top = `${y - popup.offsetHeight - 30}px`
+                popupArrow.classList.add('bottom-right')
+            } else if (!bottom && !right) {
+                // top left
+                popup.style.left = `${x - 30}px`
+                popup.style.top = `${y + 30}px`
+                popupArrow.classList.add('top-left')
+            } else {
+                // top right
+                popup.style.left = `${x - popup.offsetWidth + 30}px`
+                popup.style.top = `${y + 30}px`
+                popupArrow.classList.add('top-right')
+            }
         }
     })
     map.on('mouseleave', 'state-population', function () {
@@ -211,14 +134,8 @@ function createPopupMsg(data) {
     }
 
     return `
-        <h2>${data.properties.GEN}, ${data.properties.BEZ}</h2>
-        ${data.properties.EWZ ? `<p>Einwohner 2017:<br/><strong>${data.properties.EWZ}</strong></p>` : 'Keine Daten vorhanden'}
-        ${data.properties.JVA ? `<p>jährliche Veränderung seit 2011:<p ${sign}${data.properties.JVA}%</p></p>` : ''}
+        <div class="popup__arrow"></div>
+        <h2 class="popup__title">${data.properties.GEN}, ${data.properties.BEZ}</h2>
+        ${data.properties.EWZ ? `<p class="popup__info">Einwohner 2017:<br/><strong class="popup__info__people">${data.properties.EWZ}</strong></p><p class="popup__info">jährliche Veränderung seit 2011:<p ${data.properties.JVA ? `${sign}${data.properties.JVA}%` : 'class="black">±0%'}</p></p>` : '<p>Keine Daten vorhanden</p>'}
     `
-}
-
-function generateLegendGradient(currentColor) {
-    console.log(currentColor)
-    console.log(colorSchemes[currentColor])
-    return `linear-gradient(to right, ${colorSchemes[currentColor][1]} 0%, ${colorSchemes[currentColor][5]} 33%, ${colorSchemes[currentColor][9]} 49%, ${colorSchemes[currentColor][11]} 50%, ${colorSchemes[currentColor][13]} 51%, ${colorSchemes[currentColor][17]} 67%, ${colorSchemes[currentColor][21]} 100%)`
 }
